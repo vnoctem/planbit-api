ea4f3f32adaa83aefd4cc12f2ef67ed0fa5747fe
Vincent <vincent.ngo.gauthier@gmail.com>
2021-11-11 21:09:50 -0500

    Initial commit

---
+++ src/main/java/com/planbit/api/PlanbitApiApplication.java
@@ -0,0 +1,13 @@
+package com.planbit.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlanbitApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlanbitApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/planbit/api/PlanbitApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.planbit.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlanbitApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
